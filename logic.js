39951eeb1a9964614a51a4dcf0cba01dd51fc656
vaishnaviyerge <135841337+vaishnaviyerge@users.noreply.github.com>
2023-07-28 13:31:55 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,28 @@
+function Calculate()
+{
+    var height = document.getElementById("h-input").value;
+    var weight = document.getElementById("w-input").value;
+
+    var result = parseFloat(weight) /(parseFloat(height)/100)**2;
+
+    if(!isNaN(result))
+    {
+        document.getElementById("bmi-output").innerHTML = result;
+        if(result < 18.5)
+        {
+            document.getElementById("bmi-status").innerHTML = "Underweight😑";
+        }
+        else if(result < 25)
+        {
+            document.getElementById("bmi-status").innerHTML = "Healthy💛";
+        }
+        else if(result < 30)
+        {
+            document.getElementById("bmi-status").innerHTML = "Overweight😦";
+        }
+        else
+        {
+            document.getElementById("bmi-status").innerHTML = "Obesity😮";
+        }
+    }
+}
